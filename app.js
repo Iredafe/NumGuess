af44ee1f26f91675f06f467dfbe74447aa98e8e7
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-08 22:11:08 -0600

    building vars

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+//game values
+let min=1, max = 10, winning = 2, guessesLeft = 3;
+
+//ui elements
+const game = document.getElementById('game'),
+minNum = document.querySelector('.min-num'),
+maxnum = document.querySelector('.max-num')
