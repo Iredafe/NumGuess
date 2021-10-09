dc1c1142df91f7723c8eb7d9588285b759530ef7
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-08 22:40:29 -0600

    fix bug

--- app.js
+++ app.js
@@ -4,12 +4,17 @@
 let min=1, max = 10, winningNum = 2, guessesLeft = 3;
 
 //ui elements
-const game = document.getElementById('.#game'),
+const game = document.getElementById('#game'),
 minNum = document.querySelector('.min-num'),
 maxNum = document.querySelector('.max-num'),
-guessBtn = document.querySelector('.#guess-input'),
+guessBtn = document.querySelector('#guess-btn'),
 message = document.querySelector('.message');
 
 //assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+//listener for guess
+guessBtn.addEventListener('click', function(){
+    console.log(guessBtn.value);
+})
