b2a00610c93a8041d08fba2b651a05b784146948
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-08 22:25:25 -0600

    building vars II

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 
 
 //game values
-let min=1, max = 10, winning = 2, guessesLeft = 3;
+let min=1, max = 10, winningNum = 2, guessesLeft = 3;
 
 //ui elements
-const game = document.getElementById('game'),
+const game = document.getElementById('.#game'),
 minNum = document.querySelector('.min-num'),
-maxnum = document.querySelector('.max-num')
+maxNum = document.querySelector('.max-num'),
+guessBtn = document.querySelector('.#guess-input'),
+message = document.querySelector('.message');
+
+//assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
